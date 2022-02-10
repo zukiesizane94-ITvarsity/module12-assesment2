87c49f82aff80f32b641911bbaab946672eab9b3
zukiesizane <85501805+zukiesizane@users.noreply.github.com>
2022-02-10 08:22:52 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
+let apiKey  = checkApiKey();
+
+function checkApiKey() {
+    if (!localStorage.getItem("apiKey")){
+        window.open("enter-api-key.html", "_self");
+    }
+    return localStorage.getItem("apiKey");
+}
